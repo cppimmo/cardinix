70c2beaca429c59e5be9dcd78db4c9329b7dab01
cppimmo <immortalitybh@gmail.com>
2021-07-13 00:11:00 -0500

    fix header name collisions in libc

--- libc/string.h
+++ libc/string.h
@@ -33,10 +33,14 @@ size_t strxfrm(char *dest, const char *src, size_t n);
 
 size_t strlen(const char *str)
 {
-	if (str != 0)
+	/*if (str != 0)
 		return sizeof(str)/sizeof(str[0]);
 	else
-		return 0;
+		return 0;*/
+	size_t len = 0;
+	while (str[len])
+		++len;
+	return len;
 }
 
 #ifdef __cplusplus
